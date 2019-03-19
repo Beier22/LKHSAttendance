fee12bc5ecf54778db95eb4b51e59091e532ea01
Mads <mads_beier@hotmail.com>
2019-03-19 14:57:41 +0100

    Update UserDAO.java

--- src/lkhsattendance/dal/UserDAO.java
+++ src/lkhsattendance/dal/UserDAO.java
@@ -6,6 +6,8 @@
 package lkhsattendance.dal;
 
 import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 import lkhsattendance.be.Student;
 import lkhsattendance.be.Subject;
@@ -20,11 +22,23 @@ public class UserDAO {
     SQLServerDataSource ds = dba.DBAccess();
     
     public List<Student> getAllStudents(){
-        return null; //TODO
+        try (Connection con = ds.getConnection()){
+            //TODO
+        } catch (SQLException sqle) {
+            System.out.println("Error connecting to database");
+        }
+        
+        return null;
     }
     
     public List<Subject> getAllSubjects(){
-        return null; //TODO
+        try (Connection con = ds.getConnection()){
+            //TODO
+        } catch (SQLException sqle) {
+            System.out.println("Error connecting to database");
+        }
+        
+        return null;
     }
     
 }
